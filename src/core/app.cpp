9b2fce452c24ea5b3ba0004916256e91eb004c40
DevManDan1178 <l3r0d4niel@gmail.com>
2026-06-21 18:09:32 -0400

    testing audio file selection and playing on load

--- src/core/app.cpp
+++ src/core/app.cpp
@@ -1,8 +1,18 @@
+#include <filesystem>
+#include <iostream>
 #include "core/app.hpp"
 #include "ui/file_dialog.hpp"
-
+#include "audio/audio_manager.hpp"
+#include "audio/audio.hpp"
 
 void App::Initialize() {
-    FileDialog::OpenAudioFile();
+    AudioManager audioManager;
+    
+    std::filesystem::path filePath = FileDialog::OpenAudioFile();
+    Audio audio(filePath);
+    std::cout << "Attempting to play audio [" << audio.audioPath << "] at volume [" << audio.volume << "]" << std::endl;
+    audioManager.LoadAudio(audio);
+    audioManager.PlayAudio(audio);
+    std::cin.get();
     return;
 }
