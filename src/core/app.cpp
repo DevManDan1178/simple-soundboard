28aa1d3f328d0b83de6631f3f2e494a55da36e8d
DevManDan1178 <l3r0d4niel@gmail.com>
2026-06-23 00:20:32 -0400

    + implementation

--- src/core/app.cpp
+++ src/core/app.cpp
@@ -2,17 +2,163 @@
 #include <iostream>
 #include "core/app.hpp"
 #include "ui/file_dialog.hpp"
+#include "ui/configuration_UI.hpp"
 #include "audio/audio_manager.hpp"
 #include "audio/audio.hpp"
 
+#include "SDL.h"
+#include <SDL_opengl.h>
+#include <SDL_syswm.h>
+
+#include "imgui.h"
+#include "backends/imgui_impl_sdl2.h"
+#include "backends/imgui_impl_opengl3.h"
+
+#include "ui/wheel_UI.hpp"
+
+void App::UI() {
+
+    ImGui::SetNextWindowBgAlpha(0.0f);
+    ImGui::SetNextWindowPos(ImVec2(0, 0));
+    ImGui::SetNextWindowSize(ImGui::GetIO().DisplaySize);
+
+    ImGui::Begin(
+        "Soundboard",
+        nullptr,
+        ImGuiWindowFlags_NoTitleBar |
+        ImGuiWindowFlags_NoResize   |
+        ImGuiWindowFlags_NoMove     
+    );
+
+    ImGui::Text("- Audio Controls");
+    ConfigurationUI::Render(soundboard);
+    ImGui::Text("----");
+   
+    ImGui::End();
+}
+
+void setClickThrough(HWND hwnd, bool clickThrough) {
+    LONG_PTR exStyle = GetWindowLongPtr(hwnd, GWL_EXSTYLE);
+    if (clickThrough)
+        exStyle |= WS_EX_TRANSPARENT;
+    else
+        exStyle &= ~WS_EX_TRANSPARENT;
+    SetWindowLongPtr(hwnd, GWL_EXSTYLE, exStyle);
+}
+
+void setBorderless(SDL_Window* window, HWND hwnd, bool borderless)
+{
+    SDL_SetWindowBordered(window, borderless ? SDL_FALSE : SDL_TRUE);
+
+    // force Windows to apply the style change
+    SetWindowPos(hwnd, nullptr, 0, 0, 0, 0,
+        SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER | SWP_FRAMECHANGED);
+}
+
+
+void App::InitializeUI() {
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) != 0) {
+        std::cout << "SDL Init failed: " << SDL_GetError() << std::endl;
+        return;
+    }
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+    SDL_Window* window = SDL_CreateWindow(
+        "Soundboard",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        600, 600,
+        SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE
+    );
+
+    SDL_SysWMinfo wmInfo{};
+    SDL_VERSION(&wmInfo.version);
+    SDL_GetWindowWMInfo(window, &wmInfo);
+    HWND hwnd = wmInfo.info.win.window;
+
+    LONG_PTR exStyle = GetWindowLongPtr(hwnd, GWL_EXSTYLE);
+    exStyle |= WS_EX_LAYERED;
+    SetWindowLongPtr(hwnd, GWL_EXSTYLE, exStyle);
+    SetLayeredWindowAttributes(hwnd, 0, 255, LWA_ALPHA);
+    SetWindowPos(hwnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE);
+
+    int display_w, display_h;
+    SDL_GL_GetDrawableSize(window, &display_w, &display_h);
+    glViewport(0, 0, display_w, display_h);
+
+    SDL_GLContext gl_context = SDL_GL_CreateContext(window);
+    SDL_GL_MakeCurrent(window, gl_context);
+    SDL_GL_SetSwapInterval(1);
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGui::StyleColorsDark();
+
+    ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
+    ImGui_ImplOpenGL3_Init("#version 330");
+
+    bool running = true;
+    SDL_Event event;
+    while (running) {
+        if (!(GetAsyncKeyState(VK_LBUTTON) & 0x8000))
+            SetWindowPos(hwnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE);
+
+        while (SDL_PollEvent(&event)) {
+            ImGui_ImplSDL2_ProcessEvent(&event);
+            if (event.type == SDL_QUIT)
+                running = false;
+            if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE)
+                running = false;
+            if (event.type == SDL_WINDOWEVENT &&
+                event.window.event == SDL_WINDOWEVENT_SIZE_CHANGED)
+            {
+                int w = event.window.data1;
+                int h = event.window.data2;
+                glViewport(0, 0, w, h);
+            }
+        }
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplSDL2_NewFrame();
+        ImGui::NewFrame();
+
+        
+        UI();
+
+        ImGui::Render();
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(0.f, 0.f, 0.f, 1.f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        SDL_GL_SwapWindow(window);
+    }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplSDL2_Shutdown();
+    ImGui::DestroyContext();
+
+    SDL_GL_DeleteContext(gl_context);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
+
+
 void App::Initialize() {
+    InitializeUI();
+    return;
     AudioManager audioManager;
     
     std::filesystem::path filePath = FileDialog::OpenAudioFile();
     Audio audio(filePath);
-    std::cout << "Attempting to play audio [" << audio.audioPath << "] at volume [" << audio.volume << "]" << std::endl;
+    std::cout << "Attempting to play audio [" << audio.filePath << "] at volume [" << audio.volume << "]" << std::endl;
     audioManager.LoadAudio(audio);
     audioManager.PlayAudio(audio);
     std::cin.get();
     return;
 }
+
--- src/core/app.hpp
+++ src/core/app.hpp
@@ -1,7 +1,25 @@
 #pragma once
+#include "soundboard/soundboard.hpp"
+#include "config/config_manager.hpp"
 
 class App {
-    public:
-        void Initialize();
+public:
+    Soundboard soundboard;
+    ConfigManager configManager;
 
+    /**
+     * @brief Initializes the app (keeps running until application end)
+     */
+    void Initialize();
+
+private:
+    /**
+     * @brief Initializes the UI loop (keeps running until application end)
+     */
+    void InitializeUI();
+    /**
+     * @brief The UI loop called ever frame
+     */
+    void UI();
+    bool hiddenUI = false;
 };
