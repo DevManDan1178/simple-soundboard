a70c6c9dbef7439fe9aac0128ad4f2da72e4c3c2
DevManDan1178 <l3r0d4niel@gmail.com>
2026-06-22 22:31:33 -0400

    + name field, comparing

--- src/audio/audio.hpp
+++ src/audio/audio.hpp
@@ -1,10 +1,20 @@
 #pragma once
 #include <filesystem>
+#include <string>
 
 struct Audio {
-    std::filesystem::path audioPath;
+    std::filesystem::path filePath;
     float volume;
+    std::string name;
 
-    Audio(std::filesystem::path path) : audioPath(path), volume(1.0f) {}
+    Audio(std::filesystem::path path, float vol = 1.0f) : filePath(path), volume(vol) {
+        name = path.string();
+    }
+    
+    Audio(std::filesystem::path path, float vol, std::string audioName) : filePath(path), volume(vol), name(audioName) {}
+
+    bool operator==(const Audio& other) const {
+        return other.name == name  && other.filePath == filePath && other.volume == volume;
+    }
 };
 
