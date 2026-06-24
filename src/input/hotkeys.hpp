3ab579468e7e725074cff519eb67af19824ff44f
DevManDan1178 <l3r0d4niel@gmail.com>
2026-06-23 23:05:18 -0400

    + operator overload for bitmasking

--- src/input/hotkeys.hpp
+++ src/input/hotkeys.hpp
@@ -1,18 +1,24 @@
 #pragma once
+#include "SDL.h"
 
 enum class BindType {
-    KEYBOARD, MOUSE, NONE
+    Keyboard, Mouse, None
 };
 
 enum class Modifiers : unsigned int {
-    NONE = 0,
-    SHIFT = 1u << 0,
-    CTRL = 1u << 1,
-    ALT = 1u << 2,
+    None = 0,
+    Shift = 1u << 0,
+    Ctrl = 1u << 1,
+    Alt = 1u << 2,
 };  
 
 constexpr Modifiers operator|(Modifiers l, Modifiers r) {
     return static_cast<Modifiers>(static_cast<unsigned int>(l) | static_cast<unsigned int>(r));
+}
+
+constexpr Modifiers& operator|=(Modifiers& l, Modifiers r) {
+    l = l | r;
+    return l;
 }
 
 constexpr Modifiers operator&(Modifiers l, Modifiers r) {
@@ -25,8 +31,13 @@ struct Hotkey {
     Modifiers modifiers;
     
     Hotkey()
-        : keyCode(0), bindType(BindType::NONE), modifiers() {}
+        : keyCode(0), bindType(BindType::None), modifiers() {}
 
     Hotkey(int code, BindType type, Modifiers mods) : keyCode(code), bindType(type), modifiers(mods) {}
 
+    constexpr static SDL_Scancode SDL_MODIFIER_CODES[6] = {
+        SDL_SCANCODE_LSHIFT, SDL_SCANCODE_RSHIFT,
+        SDL_SCANCODE_LCTRL, SDL_SCANCODE_RCTRL,
+        SDL_SCANCODE_LALT, SDL_SCANCODE_RALT
+    };
 };
