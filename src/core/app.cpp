f3671ca3ecffda08353c8295bf05b3c205094270
DevManDan1178 <l3r0d4niel@gmail.com>
2026-06-25 00:10:11 -0400

    + event handling

--- src/core/app.cpp
+++ src/core/app.cpp
@@ -1,10 +1,13 @@
 #include <filesystem>
 #include <iostream>
+#include <functional>
+
 #include "core/app.hpp"
 #include "ui/file_dialog.hpp"
 #include "ui/configuration_UI.hpp"
 #include "audio/audio_manager.hpp"
 #include "audio/audio.hpp"
+#include "event/event_dispatcher.hpp"
 
 #include "SDL.h"
 #include <SDL_opengl.h>
@@ -16,8 +19,21 @@
 
 #include "ui/wheel_UI.hpp"
 
-void App::UI() {
+void App::ShowWheel(std::vector<std::string> elements) {
+    ImGuiViewport* viewport = ImGui::GetMainViewport();
 
+    ImVec2 screenCenter = ImVec2(
+        viewport->Pos.x + viewport->Size.x * 0.5f,
+        viewport->Pos.y + viewport->Size.y * 0.5f
+    );
+
+    // render
+    int choice = WheelUI::RenderSelectionWheel(elements, wheelVisible, screenCenter);
+}
+
+
+
+void App::UI() {
     ImGui::SetNextWindowBgAlpha(0.0f);
     ImGui::SetNextWindowPos(ImVec2(0, 0));
     ImGui::SetNextWindowSize(ImGui::GetIO().DisplaySize);
@@ -29,31 +45,16 @@ void App::UI() {
         ImGuiWindowFlags_NoResize   |
         ImGuiWindowFlags_NoMove     
     );
+    
+    if (wheelVisible) {
+       ShowWheel({"A", "B", "C", "D"});
+    }
 
-    ImGui::Text("- Audio Controls");
     ConfigurationUI::Render(soundboard);
-    ImGui::Text("----");
    
     ImGui::End();
 }
 
-void setClickThrough(HWND hwnd, bool clickThrough) {
-    LONG_PTR exStyle = GetWindowLongPtr(hwnd, GWL_EXSTYLE);
-    if (clickThrough)
-        exStyle |= WS_EX_TRANSPARENT;
-    else
-        exStyle &= ~WS_EX_TRANSPARENT;
-    SetWindowLongPtr(hwnd, GWL_EXSTYLE, exStyle);
-}
-
-void setBorderless(SDL_Window* window, HWND hwnd, bool borderless)
-{
-    SDL_SetWindowBordered(window, borderless ? SDL_FALSE : SDL_TRUE);
-
-    // force Windows to apply the style change
-    SetWindowPos(hwnd, nullptr, 0, 0, 0, 0,
-        SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER | SWP_FRAMECHANGED);
-}
 
 
 void App::InitializeUI() {
@@ -142,23 +143,49 @@ void App::InitializeUI() {
     ImGui_ImplSDL2_Shutdown();
     ImGui::DestroyContext();
 
+   
+
     SDL_GL_DeleteContext(gl_context);
     SDL_DestroyWindow(window);
     SDL_Quit();
 }
 
 
+void App::HandleEvent(const Event& event) {
+    if (event.getType() == EventType::ToggleWheel){
+        std::cout << "Request to open wheel" << std::endl;
+        const ToggleWheelEvent& toggleWheelEvent = dynamic_cast<const ToggleWheelEvent&>(event);
+        wheelVisible = toggleWheelEvent.visible;
+    }
+};
+
+
 void App::Initialize() {
+    InputListener::Start();
+    EventDispatcher::Subscribe([this](const Event& event) -> void {
+        HandleEvent(event);
+    });
     InitializeUI();
-    return;
-    AudioManager audioManager;
-    
-    std::filesystem::path filePath = FileDialog::OpenAudioFile();
-    Audio audio(filePath);
-    std::cout << "Attempting to play audio [" << audio.filePath << "] at volume [" << audio.volume << "]" << std::endl;
-    audioManager.LoadAudio(audio);
-    audioManager.PlayAudio(audio);
-    std::cin.get();
+    InputListener::Stop();
     return;
 }
 
+/*
+void setClickThrough(HWND hwnd, bool clickThrough) {
+    LONG_PTR exStyle = GetWindowLongPtr(hwnd, GWL_EXSTYLE);
+    if (clickThrough)
+        exStyle |= WS_EX_TRANSPARENT;
+    else
+        exStyle &= ~WS_EX_TRANSPARENT;
+    SetWindowLongPtr(hwnd, GWL_EXSTYLE, exStyle);
+}
+
+void setBorderless(SDL_Window* window, HWND hwnd, bool borderless)
+{
+    SDL_SetWindowBordered(window, borderless ? SDL_FALSE : SDL_TRUE);
+
+    // force Windows to apply the style change
+    SetWindowPos(hwnd, nullptr, 0, 0, 0, 0,
+        SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER | SWP_FRAMECHANGED);
+}
+*/
--- src/core/app.hpp
+++ src/core/app.hpp
@@ -1,12 +1,15 @@
 #pragma once
 #include "soundboard/soundboard.hpp"
 #include "config/config_manager.hpp"
+#include "event/event.hpp"
 
 class App {
 public:
     Soundboard soundboard;
     ConfigManager configManager;
 
+    bool wheelVisible = false;
+    unsigned int currentWheelIdx = 0;
     /**
      * @brief Initializes the app (keeps running until application end)
      */
@@ -17,6 +20,17 @@ private:
      * @brief Initializes the UI loop (keeps running until application end)
      */
     void InitializeUI();
+
+    /**
+     * @brief Handles events from the event dispatcher (input detection)
+     * @param Event event object
+     */
+    void HandleEvent(const Event& event);
+    /**
+     * @brief Shows the wheel UI
+     * @param elements elements to show in the wheel
+     */
+    void ShowWheel(std::vector<std::string> elements);
     /**
      * @brief The UI loop called ever frame
      */
