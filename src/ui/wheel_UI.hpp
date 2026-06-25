04cf37acb23f44c336f5bcc6c902f623add6d088
DevManDan1178 <l3r0d4niel@gmail.com>
2026-06-25 00:11:48 -0400

    ~ name adjustments and comments

--- src/ui/wheel_UI.hpp
+++ src/ui/wheel_UI.hpp
@@ -4,5 +4,12 @@
 #include "imgui.h"
 
 namespace WheelUI {
-    int RenderSelectionWheel(const std::vector<std::string>& emotes, bool& open, ImVec2 center, float radius = 120.0f); 
+    /**
+     * Renders the selection wheel and returns the index of the element being hovered (-1 if none)
+     * @param elements elements on the wheel
+     * @param openParameter reference to the boolean that controls if it is open
+     * @param center center position of the wheel
+     * @param radius radius of the wheel (default = 120.0f)
+     */
+    int RenderSelectionWheel(const std::vector<std::string>& elements, bool& openParameter, ImVec2 center, float radius = 120.0f); 
 };
