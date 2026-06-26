2071f849dc51eb4926a906ee7fafc8d680b2548b
DevManDan1178 <l3r0d4niel@gmail.com>
2026-06-26 00:57:09 -0400

    + config manager object for reading/writing saves

--- src/config/config_manager.cpp
+++ src/config/config_manager.cpp
@@ -1,34 +1,149 @@
 #include "config/config_manager.hpp"
+#include "event/event_dispatcher.hpp"
 #include <nlohmann/json.hpp>
+#include <windows.h>
+#include <iostream>
 #include <fstream>
 #include <filesystem>
 
 using json = nlohmann::json;
 
-const char* PATH_KEY = "path";
-const char* VOLUME_KEY = "volume";
+constexpr const char* PATH_KEY = "path";
+constexpr const char* VOLUME_KEY = "volume";
+constexpr const char* NAME_KEY = "name";
 
-void ConfigManager::WriteToJSON() {
-    
+constexpr const char* MASTER_VOLUME_KEY = "masterVolume";
+constexpr const char* AUDIO_TABLE_KEY = "audioTable";
+constexpr const char* OPEN_WHEEL_KEYBIND_KEY = "openWheelKeybind";
+
+static std::filesystem::path getExeDir()
+{
+    std::wstring buffer(32767, L'\0');
+
+    DWORD len = GetModuleFileNameW(
+        NULL,
+        buffer.data(),
+        static_cast<DWORD>(buffer.size())
+    );
+
+    if (len == 0)
+    {
+        return std::filesystem::current_path(); // fallback
+    }
+
+    buffer.resize(len);
+
+    return std::filesystem::path(buffer).parent_path();
 }
 
-ConfigData ConfigManager::ReadFromJSON() {
-    ConfigData data;
-    
-
-    return data;
+static std::filesystem::path getConfigPath()
+{
+    return getExeDir() / "config" / "config.json";
 }
 
-json ConfigManager::AudioToJSON(const Audio &audio) {
+void ConfigManager::WriteToJSON() const {
+    try {
+        std::filesystem::create_directories(getConfigPath().parent_path());
+        std::ofstream file(getConfigPath());
+        if (!file.is_open()) {
+            std::cout << "Failed to open file for writing: " << getConfigPath() << std::endl;
+            return;
+        }
+
+        AudioManager& audioManager = soundboard.audioManager;
+        AudioTable& audioTable = soundboard.audioTable;
+        HotkeyManager& hotkeyManager = soundboard.hotkeyManager;
+
+        json jsonData;
+
+        jsonData[MASTER_VOLUME_KEY] = audioManager.masterVolume;
+        jsonData[OPEN_WHEEL_KEYBIND_KEY] = HotkeyToJSON(hotkeyManager.openWheelHotkey);
+
+        for (int tableIdx = 0; tableIdx < audioTable.size(); tableIdx++) {
+            std::vector<Audio>& audios = *audioTable.GetAudiosAtIdx(tableIdx);
+            for (int audioIdx = 0; audioIdx < audios.size(); audioIdx++) {
+                jsonData[AUDIO_TABLE_KEY][tableIdx][audioIdx] = AudioToJSON(audios[audioIdx]);
+            }
+        }
+
+        file << jsonData.dump(4);
+    } catch (const std::exception& e) {
+        std::cout << "Error writing to JSON: " << e.what() << std::endl;
+    }
+}
+
+void ConfigManager::UpdateSoundboardFromJSON() {
+    try {
+        std::filesystem::create_directories(getConfigPath().parent_path());
+        std::ifstream file(getConfigPath());
+        if (!file.is_open()) {
+            std::cout << "Failed to open file at path " << getConfigPath() << std::endl;
+            return;
+        }
+        AudioManager& audioManager = soundboard.audioManager;
+        AudioTable& audioTable = soundboard.audioTable;
+        HotkeyManager& hotkeyManager = soundboard.hotkeyManager;
+
+        json jsonData;
+        file >> jsonData;
+
+        audioManager.masterVolume = jsonData[MASTER_VOLUME_KEY].get<float>();
+        hotkeyManager.openWheelHotkey = JSONToHotkey(jsonData[OPEN_WHEEL_KEYBIND_KEY]);
+
+        audioTable.Clear();
+        for (int tableIdx = 0; tableIdx < jsonData[AUDIO_TABLE_KEY].size(); tableIdx++) {
+            audioTable.CreateSubTable();
+            auto& audioList = jsonData[AUDIO_TABLE_KEY][tableIdx];
+            for (int audioIdx = 0; audioIdx < audioList.size(); audioIdx++) {
+                Audio audio = JSONToAudio(audioList[audioIdx]);
+                audioTable.AddAudio(audio, tableIdx);
+            }
+        }
+
+    }   catch (const std::exception& e) {
+        std::cout << "Error reading from JSON: " << e.what() << std::endl;
+    }
+}
+
+
+ConfigManager::ConfigManager(Soundboard& sb) : soundboard(sb) {
+    UpdateSoundboardFromJSON();
+    EventDispatcher::Subscribe([this](const Event& event) -> void {
+        if (event.getType() == EventType::ConfigChange) {
+            WriteToJSON();
+        }
+    });
+}
+
+json ConfigManager::AudioToJSON(const Audio &audio) const {
     return {
         {PATH_KEY, audio.filePath.string()},
-        {VOLUME_KEY, audio.volume}
+        {NAME_KEY, audio.name},
+        {VOLUME_KEY, audio.volume},
     };
 }
 
-Audio ConfigManager::JSONToAudio(const json &json) {
+Audio ConfigManager::JSONToAudio(const json &json) const {
     std::filesystem::path path(json[PATH_KEY].get<std::string>());
     float volume = json[VOLUME_KEY].get<float>();
+    std::string name = json[NAME_KEY].get<std::string>();
+    return Audio(path, volume, name);
+}
 
-    return Audio(path, volume);
+nlohmann::json ConfigManager::HotkeyToJSON(const Hotkey& hotkey) const {
+    json jsonData;
+    jsonData["keyCode"] = hotkey.keyCode;
+    jsonData["modifiers"] = hotkey.modifiers;
+    jsonData["bindType"] = hotkey.bindType == BindType::Keyboard ? "Keyboard" : (hotkey.bindType == BindType::Mouse ? "Mouse" : "None");
+    return jsonData;
+}
+
+
+Hotkey ConfigManager::JSONToHotkey(const nlohmann::json& json) const {
+    Hotkey hotkey;
+    hotkey.keyCode = json["keyCode"].get<int>();
+    hotkey.modifiers = Modifiers{json["modifiers"].get<unsigned int>()};
+    std::string bindType = json["bindType"].get<std::string>();
+    hotkey.bindType = bindType == "Keyboard" ? BindType::Keyboard : (bindType == "Mouse" ? BindType::Mouse : BindType::None);
+    return hotkey;
 }
--- src/config/config_manager.hpp
+++ src/config/config_manager.hpp
@@ -1,17 +1,45 @@
 #pragma once
-#include "../audio/audio.hpp"
+#include "soundboard/soundboard.hpp"
+#include "input/hotkeys.hpp"
 #include <nlohmann/json.hpp>
-
-struct ConfigData {
-    std::vector<std::vector<Audio>> audioTable;
-};
 
 class ConfigManager {
 public:
-    void WriteToJSON();
-    ConfigData ReadFromJSON();
-
+    void WriteToJSON() const;
+    ConfigManager(Soundboard& soundboard);
 private:
-    nlohmann::json AudioToJSON(const Audio& audio);
-    Audio JSONToAudio(const nlohmann::json& json);
+    Soundboard& soundboard;
+    
+    /**
+     * @brief updates the soundboard by reading from a JSON file
+     */
+    void UpdateSoundboardFromJSON();
+    
+    /**
+     * @brief Converts Audio to nlohmann JSON
+     * @param audio audio struct
+     * @return nlohmann JSON of audio
+     */
+    nlohmann::json AudioToJSON(const Audio& audio) const;
+    
+    /**
+     * @brief Converts nlohmann JSON into audio
+     * @param json JSONified audio struct
+     * @return audio struct from the json
+     */
+    Audio JSONToAudio(const nlohmann::json& json) const;
+
+    /**
+     * @brief Converts hotkey to nlohmann JSON
+     * @param hotkey hotkey struct
+     * @return nlohmann JSON of hotkey
+     */
+    nlohmann::json HotkeyToJSON(const Hotkey& hotkey) const;
+
+    /**
+     * @brief Converts nlohmann JSON into hotkey
+     * @param json JSONified hotkey struct
+     * @return hotkey struct from the json
+     */
+    Hotkey JSONToHotkey(const nlohmann::json& json) const;
 };
